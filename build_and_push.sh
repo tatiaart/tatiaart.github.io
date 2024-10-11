00766e220ba04c68616ceeaf25c8ca387196d6b0
pepfof <artem.tatianin@st.com>
2024-10-11 14:37:20 +0200

    removed unneeded md, updated script

--- build_and_push.sh
+++ build_and_push.sh
@@ -6,14 +6,16 @@ tmp_dir=$(mktemp -d -t site-build-XXXX)
 cd "$tmp_dir" || exit
 git clone git@github.com:tatiaart/tatiaart.github.io.git
 cd tatiaart.github.io || exit
-git branch gh-pages || git checkout gh-pages
+git branch gh-pages
+git checkout gh-pages
 mv "$tmp_dir/tatiaart.github.io/.git" "$tmp_dir/.git"
 rm -rf "$tmp_dir/tatiaart.github.io"
 cp -r "$site_dir/_site" "$tmp_dir/tatiaart.github.io"
 mv "$tmp_dir/.git" "$tmp_dir/tatiaart.github.io/.git"
 cd "$tmp_dir/tatiaart.github.io" || exit
 git add -A
-git commit -m "$(date +"%T")"
+git commit -m "$(date +"%s")"
 ssh-agent sh -c 'ssh-add ~/.ssh/tatiaart.key; git push'
 cd "$site_dir" || exit
+rm -rf "$tmp_dir"
 echo "done"
